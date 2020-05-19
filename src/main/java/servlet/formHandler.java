3d15f0daa8f3e0d817487a96bacad181c183f8ee
TWRphantom <54685570+TWRphantom@users.noreply.github.com>
2020-05-19 15:09:39 -0400

    real consequences

--- src/main/java/servlet/formHandler.java
+++ src/main/java/servlet/formHandler.java
@@ -1,131 +1,166 @@
-/** File name: formHandler.java 
- * 
- *  Author:    Quansheng Xiao, Jeff Offutt
- */
+/** *****************************************************************
+    twoButtons.java   servlet example
 
-// Import Servlet Libraries
-import javax.servlet.*;
-import javax.servlet.http.*;
+        @author Jeff Offutt
+********************************************************************* */
 
 // Import Java Libraries
 import java.io.*;
 import java.util.*;
-import java.lang.*;
 
-// formHandler class
-// Generic form handler -- Echo all the parameters and values
-//     that a client inputs from an HTML form.
-// Note: the name of the submit button in the form must be "submit"
-// (ignore case) or the servlet will print "submit" parameter.
+//Import Servlet Libraries
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-
+// twoButtons class
 // CONSTRUCTOR: no constructor specified (default)
 //
-// ****************  Methods description  *******************************
-// void doPost ()    --> Main method for gathering data and sending back
-// void doGet ()     --> Not used.
+// ***************  PUBLIC OPERATIONS  **********************************
+// public void doPost ()  --> prints a blank HTML page
+// public void doGet ()  --> prints a blank HTML page
+// private void PrintHead (PrintWriter out) --> Prints the HTML head section
+// private void PrintBody (PrintWriter out) --> Prints the HTML body with
+//              the form. Fields are blank.
+// private void PrintBody (PrintWriter out, String lhs, String rhs, String rslt)
+//              Prints the HTML body with the form.
+//              Fields are filled from the parameters.
+// private void PrintTail (PrintWriter out) --> Prints the HTML bottom
 //***********************************************************************
-public class formHandler extends HttpServlet
+
+public class twoButtons extends HttpServlet
 {
 
-/** **********************************************************
- *  doPost()
- *  gather data and respond to browser
- ************************************************************ */
-public void doPost(HttpServletRequest request, HttpServletResponse response)
-       throws ServletException, IOException
-{   
-   // first, set the "content type" header of the response
-   response.setContentType ("text/html");
-   //Get the response's PrintWriter to return text to the client.
-   PrintWriter toClient = response.getWriter ();
+// Location of servlet.
+static String Domain  = "cs.gmu.edu:8443";
+static String Path    = "/offutt/servlet/";
+static String Servlet = "twoButtons";
 
-   String para;
-   Enumeration paraNames = request.getParameterNames();
+// Button labels
+static String OperationAdd = "Add";
+static String OperationSub = "Subtract";
 
-   toClient.println("<html>");
-   toClient.println("<head>");
-   toClient.println("  <title>Generic form handler</title>");
-   toClient.println("</head>");
-
-   toClient.println("<body bgcolor=\"#EEEEEE\">");
-   toClient.println("");
-   toClient.println("<center><h2>Generic form handler</h2></center>");
-   toClient.println("<p>");
-   toClient.println("The following table lists all parameter names and");
-   toClient.println("their values that were submitted from your form.");
-   toClient.println("</p>");
-   toClient.println("");
-   toClient.println("<p>");
-   toClient.println("<table cellSpacing=1 cellPadding=1 width=\"75%\" border=1 bgColor=lavender>");
-   toClient.println("");
-   toClient.println("  <tr bgcolor=\"#FFFFFF\">");
-   toClient.println("   <th align=\"center\"><b>Parameter</b></td>");
-   toClient.println("   <th align=\"center\"><b>Value</b></td>");
-   toClient.println("  </tr>");
-
-   while (paraNames.hasMoreElements())
-   {  // For each parameter name.
-      para = (String)paraNames.nextElement();
-      if (!para.equalsIgnoreCase("submit"))
-      {
-         toClient.println("  <tr>");
-         toClient.println("    <td style=\"width: 20%\" width=\"20%\"><b>" + para + "</b></td>");
-
-         String[] values = request.getParameterValues(para);
-
-         if (values != null && !values[0].equals(""))
-            toClient.println("    <td>" + values[0] + "</td></tr>");
-         else
-            toClient.println("    <td>&nbsp;</td></tr>");
-
-         for (int i = 1; i < values.length; i++)
-         {
-            if (!values[i].equals(""))
-            {
-               toClient.println("  <tr>");
-               toClient.println("    <td style=\"width: 20%\" width=\"20%\">&nbsp;</td>");
-               toClient.println("    <td>" + values[i] + "</td></tr>");
-            }
-         }
-      }
-   }
-   toClient.println("</table>");
-   toClient.println("");
-   toClient.println("</body>");
-   toClient.println("</html>");
-
-   toClient.println("");
-
-   // Close the writer; the response is done.
-   toClient.close();      
-} //end of doPost()
-   
+// Other strings.
+static String Style ="https://www.cs.gmu.edu/~offutt/classes/432/432-style.css";
 
 /** *****************************************************
-  *   doGet()
-  *   not used
- ********************************************************* */
-public void doGet (HttpServletRequest request,
-                   HttpServletResponse response)
+ *  Overrides HttpServlet's doPost().
+ *  Converts the values in the form, performs the operation
+ *  indicated by the submit button, and sends the results
+ *  back to the client.
+********************************************************* */
+public void doPost (HttpServletRequest request, HttpServletResponse response)
+   throws ServletException, IOException
+{
+   Float rslt   = new Float(0.0);
+   Float lhsVal = new Float(0.0);
+   Float rhsVal = new Float(0.0);
+   String operation = request.getParameter("Operation");
+   String lhsStr = request.getParameter("LHS");
+   String rhsStr = request.getParameter("RHS");
+   if ((lhsStr != null) && (lhsStr.length() > 0))
+      lhsVal = new Float(lhsStr);
+   if ((rhsStr != null) && (rhsStr.length() > 0))
+      rhsVal = new Float(rhsStr);
+
+   if (operation.equals(OperationAdd))
+   {
+      rslt = new Float(lhsVal.floatValue() + rhsVal.floatValue());
+   }
+   else if (operation.equals(OperationSub))
+   {
+      rslt = new Float(lhsVal.floatValue() - rhsVal.floatValue());
+   }
+
+   response.setContentType("text/html");
+   PrintWriter out = response.getWriter();
+   PrintHead(out);
+   PrintBody(out, lhsStr, rhsStr, rslt.toString());
+   PrintTail(out);
+}  // End doPost
+
+/** *****************************************************
+ *  Overrides HttpServlet's doGet().
+ *  Prints an HTML page with a blank form.
+********************************************************* */
+public void doGet (HttpServletRequest request, HttpServletResponse response)
        throws ServletException, IOException
 {
    response.setContentType("text/html");
-   //Get the response's PrintWriter to return text to the client.
    PrintWriter out = response.getWriter();
-            
-   String title = "This is from formHandler Servlet";
-      
-   out.println("<html>");
-   out.println("<head>");
-   out.println("  <title>" + title + "</title>");
-   out.println("</head>");
-   out.println("<body>");
-   out.println("  <h1>" + title + "</h1>");
-   out.println("  <p>Please run the servlet from formHandler.html.");
-   out.println("</body>");
-   out.println("</html>");
-   out.close();
-}  // End doGet()
+   PrintHead(out);
+   PrintBody(out);
+   PrintTail(out);
+} // End doGet
 
-}  // End formHandler class
+/** *****************************************************
+ *  Prints the <head> of the HTML page, no <body>.
+********************************************************* */
+private void PrintHead (PrintWriter out)
+{
+   out.println("<html>");
+   out.println("");
+
+   out.println("<head>");
+   out.println("<title>Two buttons example</title>");
+   out.println(" <link rel=\"stylesheet\" type=\"text/css\" href=\"" + Style + "\">");
+   out.println("</head>");
+   out.println("");
+} // End PrintHead
+
+/** *****************************************************
+ *  Prints the <BODY> of the HTML page with the form data
+ *  values from the parameters.
+********************************************************* */
+private void PrintBody (PrintWriter out, String lhs, String rhs, String rslt)
+{
+   out.println("<body>");
+   out.println("<p>");
+   out.println("A simple example that demonstrates how to operate with");
+   out.println("multiple submit buttons.");
+   out.println("</p>");
+   out.print  ("<form method=\"post\"");
+   out.println(" action=\"https://" + Domain + Path + Servlet + "\">");
+   out.println("");
+   out.println(" <table>");
+   out.println("  <tr>");
+   out.println("   <td>First value:");
+   out.println("   <td><input type=\"text\" name=\"LHS\" value=\"" + lhs + "\" size=5>");
+   out.println("  </tr>");
+   out.println("  <tr>");
+   out.println("   <td>Second value:");
+   out.println("   <td><input type=\"text\" name=\"RHS\" value=\"" + rhs + "\" size=5>");
+   out.println("  </tr>");
+   out.println("  <tr>");
+   out.println("   <td>Result:");
+   out.println("   <td><input type=\"text\" name=\"RHS\" value=\"" + rslt + "\" size=6>");
+   out.println("  </tr>");
+   out.println(" </table>");
+   out.println(" <br>");
+   out.println(" <br>");
+   out.println(" <input type=\"submit\" value=\"" + OperationAdd + "\" name=\"Operation\">");
+   out.println(" <input type=\"submit\" value=\"" + OperationSub + "\" name=\"Operation\">");
+   out.println(" <input type=\"reset\" value=\"Reset\" name=\"reset\">");
+   out.println("</form>");
+   out.println("");
+   out.println("</body>");
+} // End PrintBody
+
+/** *****************************************************
+ *  Overloads PrintBody (out,lhs,rhs,rslt) to print a page
+ *  with blanks in the form fields.
+********************************************************* */
+private void PrintBody (PrintWriter out)
+{
+   PrintBody(out, "", "", "");
+}
+
+/** *****************************************************
+ *  Prints the bottom of the HTML page.
+********************************************************* */
+private void PrintTail (PrintWriter out)
+{
+   out.println("");
+   out.println("</html>");
+} // End PrintTail
+
+}  // End twoButtons
